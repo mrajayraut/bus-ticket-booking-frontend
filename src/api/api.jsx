a62f519af3c117d2a3430ff791b104d1fcde8e62
Ajay Raut <rautajay189@gmail.com>
2026-08-05 14:44:24 +0530

    Use environment variable for API base URL

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,21 +1,21 @@
 import axios from "axios";
 
-const api=axios.create({
-    baseURL:"http://localhost:8080",
-    headers:{"Content-Type":"application/json"}
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+    headers: {"Content-Type": "application/json"}
 });
 
 api.interceptors.request.use(
-    (config)=>{
-        const token=localStorage.getItem("item");
+    (config) => {
+        const token = localStorage.getItem("item");
 
-        if(token){
-            config.headers.Authorization=`Bearer ${token}`;
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
         }
 
         return config;
     },
-    (error)=>{
+    (error) => {
         return Promise.reject(error);
     }
 );
